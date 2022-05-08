ddb1ac7b3463d13a1ab5513d951234d59ffd4a5d
yabandzhiev <yabandzhiev@abv.bg>
2022-05-08 10:06:00 +0300

    Create store.js

---
+++ src/store/store.js
@@ -0,0 +1,36 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import userReducer from "./user/userSlice";
+import vehicleReducer from "./vehicles/vehicleSlice";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({ user: userReducer, vehicles: vehicleReducer });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(logger),
+});
+
+export const persistor = persistStore(store);
