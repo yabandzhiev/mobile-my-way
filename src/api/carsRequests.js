ed6024128812cf5a0270a06522985be0df5e03f8
yabandzhiev <yabandzhiev@abv.bg>
2022-05-17 13:25:14 +0300

    create all requests from backend

--- src/api/carsRequests.js
+++ src/api/carsRequests.js
@@ -1,5 +1,42 @@
-import {axios} from 'axios';
+import { axios, CAR_SERVICE } from "../constants/backend";
 
-const createCar = async (token, user, data){
-    
-}
+const getAllCarsRequest = async () => {
+  try {
+    const vehiclesData = await axios.get(`${CAR_SERVICE}/all`);
+    return vehiclesData;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const createCarRequest = async (token, data) => {
+  try {
+    return await axios.post(CAR_SERVICE, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const editCarRequest = async (token, data, userId) => {
+  try {
+    return await axios.put(`${CAR_SERVICE}/${userId}`, data, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const deleteCarRequest = async (token, vehicleId, userId) => {
+  try {
+    return await axios.delete(`${CAR_SERVICE}/${vehicleId}/${userId}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export { getAllCarsRequest, createCarRequest, editCarRequest, deleteCarRequest };
