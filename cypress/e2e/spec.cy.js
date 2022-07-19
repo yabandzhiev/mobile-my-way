a19c6b52705108acf6fca95b914a2e898839a70d
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-19 18:47:04 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
 
 describe("First Tests", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
   it("should load homepage with  data", () => {
     cy.intercept("GET", "**/all").as("allVehicles");
-    cy.visit("/");
     cy.wait("@allVehicles");
     cy.get("@allVehicles").then((body) => {
       expect(body.response.statusCode).to.equal(200);
@@ -16,18 +19,34 @@ describe("First Tests", () => {
     const emptyArr = [];
 
     cy.intercept("GET", "**/all", { emptyArr });
-    cy.visit("/");
 
     cy.get(".MuiTable-root").should("contain", "No records to display");
   });
 
-  it.only("should login", () => {
-    cy.visit("/login");
+  it.only("should login, check localStorage and go to homepage", () => {
+    cy.get(".header").contains("LOGIN").click();
     cy.get("form").find('[name="email"]').clear().type(Cypress.env("email"));
     cy.get("form")
       .find('[name="password"]')
       .clear()
       .type(Cypress.env("password"));
+
+    cy.intercept("POST", "**/users/login").as("loginReq");
+
     cy.contains("Sign In").click();
+
+    cy.wait("@loginReq");
+    cy.get("@loginReq").then((body) => {
+      const token = body.response.body.jwtToken;
+      const userId = body.response.body.user.id;
+
+      const localStorageItems = JSON.parse(localStorage.getItem("user"));
+      const localStorageToken = localStorageItems.token;
+      const localStorageUserId = localStorageItems.id;
+
+      expect(token).to.equal(localStorageToken);
+      expect(userId).to.equal(localStorageUserId);
+      cy.url().should("eq", "http://localhost:3000/");
+    });
   });
 });
