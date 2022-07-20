842aeccb7480180d7f00dd52b59ef612bf4b6113
Ivan <50044444+yabandzhiev@users.noreply.github.com>
2022-07-20 09:28:51 +0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -47,13 +47,79 @@ describe("First Tests", () => {
     });
   });
 
-  it("should have add button after sign in", () => {
+  it.only("should have add button after sign in, add new vehicle, find it, delete it, after that log out", () => {
     cy.loginToApp();
     cy.wait("@loginReq");
-
+    //addBtn
     cy.get('button[title="Add"]').as("addBtn");
 
-    cy.get("@addBtn").should("be.visible");
+    cy.get("@addBtn").should("be.visible").click();
+    //add
+    cy.get('tr[mode="add"]')
+      .find("td")
+      .then((td) => {
+        cy.wrap(td[1]).type("Alfa");
+        cy.wrap(td[2]).type("Romeo");
+        cy.wrap(td[3]).type("1999");
+
+        cy.wrap(td[4]).find('[aria-haspopup="listbox"]').click();
+        cy.get('ul[role="listbox"]')
+          .find("li")
+          .then((li) => {
+            cy.wrap(li[0]).click();
+          });
+
+        cy.wrap(td[5]).find('[aria-haspopup="listbox"]').click();
+        cy.get('ul[role="listbox"]')
+          .find("li")
+          .then((li) => {
+            cy.wrap(li[0]).click();
+          });
+
+        cy.wrap(td[6]).find('[aria-haspopup="listbox"]').click();
+        cy.get('ul[role="listbox"]')
+          .find("li")
+          .then((li) => {
+            cy.wrap(li[0]).click();
+          });
+        cy.wrap(td[7]).type(120);
+        cy.wrap(td[8]).type("Blue");
+        cy.wrap(td[9]).type("2999");
+        cy.wrap(td[10]).type("Sofia");
+        cy.wrap(td[11]).type(299999);
+        cy.wrap(td[12]).type("AC, Parking Sensors");
+
+        cy.wrap(td[0]).find('button[title="Save"]').click();
+      });
+    cy.get("tr")
+      .should("contain", "Alfa")
+      .and("contain", "Romeo")
+      .and("contain", 1999);
+    //edit
+    cy.get("tr")
+      .contains("Alfa")
+      .prev()
+      .then((buttons) => {
+        cy.wrap(buttons).find('button[title="Edit"]').click();
+      });
+
+    cy.get('tr[mode="update"]').find('input[value="Alfa"]').type("111");
+
+    cy.get('button[title="Save"]').click();
+
+    cy.get("tr").should("contain", "Alfa111");
+
+    //delete
+    cy.get("tr")
+      .contains("Alfa111")
+      .prev()
+      .then((buttons) => {
+        cy.wrap(buttons).find('button[title="Delete"]').click();
+        cy.get('tr[mode="delete"]').find('button[title="Save"]').click();
+      });
+
+    cy.get("tr").should("not.contain", "Alfa111");
+    // //logout
     cy.get(".header")
       .contains("Logout")
       .click()
